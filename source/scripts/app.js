e4d158c654524ccb82fa5cdafd55df0f451c8371
Josh Magness <magnessjo@me.com>
2020-04-29 20:15:39 -0400

    d

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -1,11 +1,8 @@
 import '@babel/polyfill';
 import { Header } from './header';
-import properties from './properties';
 
 require('styles/pages/index.css');
 
 document.addEventListener('DOMContentLoaded', () => {
-  // properties();
-
   Header();
 });
